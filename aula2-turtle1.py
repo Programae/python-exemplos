bcd543655abbd7cc4f72b11531876cf3be8f0d5e
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-09 15:27:34 -0200

    usando plano cartesiano, adiciona imagem de fundo
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ aula2-turtle1.py
@@ -0,0 +1,30 @@
+'''
+Exemplo Aula #2
+Programaê - Python
+http://programae.org.br
+'''
+from turtle import *
+
+# define o "desenhista"
+professor = Turtle()
+bgpic("plano_cartesiano.png")
+
+# personalizações: mostra o desenho da tartaruga
+professor.shape("turtle")
+professor.showturtle()
+professor.pensize(5) # ajustando a espessura da linha
+
+professor.avance = professor.forward
+professor.recue = professor.back
+professor.direita = professor.right
+professor.esquerda = professor.left
+
+# desenha um triângulo
+professor.avance(100)
+professor.direita(60)
+professor.recue(100)
+professor.direita(60)
+professor.avance(100)
+
+# espera um clique do mouse pra sair
+exitonclick()
