32b6a47152c89d51c91e93110cea99f2e01755a4
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-09 12:06:09 -0200

    Desenha um triângulo
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ aula1-turtle2.py
@@ -0,0 +1,28 @@
+'''
+Exemplo Aula #1
+Programaê - Python
+http://programae.org.br
+'''
+from turtle import *
+
+# define o "desenhista"
+professor = Turtle()
+
+# personalizações: mostra o desenho da tartaruga
+professor.shape("turtle")
+professor.showturtle()
+
+professor.avance = professor.forward
+professor.recue = professor.back
+professor.direita = professor.right
+professor.esquerda = professor.left
+
+# desenha um triângulo
+professor.avance(100)
+professor.direita(60)
+professor.recue(100)
+professor.direita(60)
+professor.avance(100)
+
+# espera um clique do mouse pra sair
+exitonclick()
