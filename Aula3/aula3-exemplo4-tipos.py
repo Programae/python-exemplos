3ca4a6d25fa637b44e82bc84d69c396461229c3b
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-13 19:02:41 -0200

    tipos de dados (aula3)
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ Aula3/aula3-exemplo4-tipos.py
@@ -0,0 +1,29 @@
+
+# podemos aplicar a função type() no conteúdo
+
+print( type(4) ) 
+print( type(3.14) ) 
+print( type("a") ) 
+print( type("abcde") )
+print( type("123") ) 
+print( type(123) ) 
+
+# como também na variável em si
+
+gaveta = 5
+print( type(gaveta) ) 
+
+gaveta = 3.14
+print( type(gaveta) ) 
+
+gaveta = "a"
+print( type(gaveta) ) 
+
+gaveta = "abcde"
+print( type(gaveta) ) 
+
+gaveta = "123"
+print( type(gaveta) ) 
+
+gaveta = 123
+print( type(gaveta) ) 
