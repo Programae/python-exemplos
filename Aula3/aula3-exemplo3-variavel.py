63f0f71402af710dde6016983715433e5f75d30f
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-13 13:36:48 -0200

    Aula3 exemplos de variáveis
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ Aula3/aula3-exemplo3-variavel.py
@@ -0,0 +1,50 @@
+# exemplos de variáveis
+
+idade = 10
+
+nome = "José"
+
+nome_completo = "José da Silva"
+
+média = 7.5
+
+número_de_pontos = 10
+
+número1 = 3
+número2 = 5
+
+# imprimindo o resultado
+print(idade)
+print(nome)
+print(nome_completo)
+print(média)
+print(número_de_pontos)
+print(número1)
+print(número2)
+
+
+'''
+  Declarações ERRADAS
+  
+- (✗) não podemos declarar variáveis começando com números
+
+Exemplos:
+
+   1a = ...
+   2b = ...
+   3c = ...
+
+- (✗) não podemos utilizar símbolos da matemática (+, /, -, *, .)
+
+   nome+completo = ...
+   nome.completo = ...
+   nome+completo = ...
+   nome/completo = ...
+   nome*completo = ...
+
+- (✗) não podemos utilizar espaços em branco
+
+   nome completo = ...
+
+'''
+
