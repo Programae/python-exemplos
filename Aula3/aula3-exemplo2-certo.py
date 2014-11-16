01840aadbc62de072de816e2a1bf936ba285377c
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-16 17:00:12 -0200

    Arruma exemplo
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

--- Aula3/aula3-exemplo2-certo.py
+++ Aula3/aula3-exemplo2-certo.py
@@ -1,10 +1,6 @@
 
-número_1 = input("Digite o primeiro número: ")
-número_2 = input("Digite o segundo número: ")
-
-# convertendo p/ o tipo "número inteiro"
-número_1 = int(número_1)
-número_2 = int(número_2)
+número_1 = int( input("Digite o primeiro número: ") )
+número_2 = int( input("Digite o segundo número: ") )
 
 print(número_1 + número_2)
 
