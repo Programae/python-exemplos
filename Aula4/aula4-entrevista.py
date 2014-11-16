d1b7efe77afe406224de3afabaf8fea3f57e301f
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-16 21:27:42 -0200

    exemplo aula4 entrevista
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ Aula4/aula4-entrevista.py
@@ -0,0 +1,19 @@
+
+print("Bem-vindo ao programa de entrevistas")
+print("Por favor, responda as seguintes questões")
+
+print("Qual é o seu primeiro nome?")
+primeiroNome = input()
+
+print("Qual é o seu segundo nome?")
+segundoNome = input()
+
+print("Qual a sua idade?")
+idade = input()
+
+
+print("Obrigado por participar da pequisa. Aqui estão as respostas das perguntas:")
+print("Seu nome é: " + primeiroNome + " " + segundoNome + ". ")
+print("Sua idade é " + idade + " anos.")
+
+
