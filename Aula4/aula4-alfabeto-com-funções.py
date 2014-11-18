da13e3cd1cea1d266c01c70f1ba1b177f3429086
Tiago Maluta <tiago.maluta@gmail.com>
2014-11-18 18:04:15 -0200

    aula 4 - letras com funções
    Signed-off-by: Tiago Maluta <tiago.maluta@gmail.com>

---
+++ Aula4/aula4-alfabeto-com-funções.py
@@ -0,0 +1,193 @@
+
+def a():
+    print("      _     ")
+    print("     / \    ")
+    print("    / _ \   ")
+    print("   / ___ \  ")
+    print("  /_/___\_\ ")
+
+def b():
+    print("   ____     ")
+    print("  | __ )    ")
+    print("  |  _ \    ")
+    print("  | |_) |   ")
+    print("  |____/    ")
+
+def c():
+    print("    ____    ")
+    print("   / ___|   ")
+    print("   | |      ")
+    print("   | |___   ")
+    print("   \____|   ")
+
+def d():
+    print("   ____     ")
+    print("  |  _ \    ")
+    print("  | | | |   ")
+    print("  | |_| |   ")
+    print("  |____/    ")
+
+def e():
+    print("   _____    ")
+    print("  | ____|   ")
+    print("  |  _|     ")
+    print("  | |___    ")
+    print("  |_____|   ")
+
+def f():
+    print("   _____    ")
+    print("  |  ___|   ")
+    print("  | |_      ")
+    print("  |  _|     ")
+    print("  |_|       ")
+
+def g():
+    print("    ____    ")
+    print("   / ___|   ")
+    print("  | |  _    ")
+    print("  | |_| |   ")
+    print("   \____|   ")
+
+def h():
+    print("   _   _    ")
+    print("  | | | |   ")
+    print("  | |_| |   ")
+    print("  |  _  |   ")
+    print("  |_|_|_|   ")
+
+def i():
+    print("   ___      ")
+    print("  |_ _|     ")
+    print("   | |      ")
+    print("   | |      ")
+    print("  |___|     ")
+
+def j():
+    print("       _    ")
+    print("      | |   ")
+    print("   _  | |   ")
+    print("  | |_| |   ")
+    print("   \___/    ")
+
+def k():
+    print("   _  _     ")
+    print("  | |/ /    ")
+    print("  | ' /     ")
+    print("  | . \     ")
+    print("  |_|\_\    ")
+
+def l():
+    print("   _        ")
+    print("  | |       ")
+    print("  | |       ")
+    print("  | |___    ")
+    print("  |_____|   ")
+
+def m():
+    print("   __  __   ")
+    print("  |  \/  |  ")
+    print("  | |\/| |  ")
+    print("  | |  | |  ")
+    print("  |_|  |_|  ")
+
+def n():
+    print("   _   _    ")
+    print("  | \ | |   ")
+    print("  |  \| |   ")
+    print("  | |\  |   ")
+    print("  |_|_\_|   ")
+
+def o():
+    print("    ___    ")
+    print("   / _ \   ")
+    print("  | | | |  ")
+    print("  | |_| |  ")
+    print("   \___/   ")
+
+def p():
+    print("   ____    ")
+    print("  |  _ \   ")
+    print("  | |_) |  ")
+    print("  |  __/   ")
+    print("  |_|      ")
+
+def q():
+    print("    ___    ")
+    print("   / _ \   ")
+    print("  | | | |  ")
+    print("  | |_| |  ")
+    print("   \__\_\  ")
+
+def r():
+    print("   ____    ")
+    print("  |  _ \   ")
+    print("  | |_) |  ")
+    print("  |  _ <   ")
+    print("  |_|_\_\  ")
+
+
+def s():
+    print("   ____    ")
+    print("  / ___|   ")
+    print("  \___ \   ")
+    print("   ___) |  ")
+    print("  |____/   ")
+
+def t():
+    print("   _____   ")
+    print("  |_   _|  ")
+    print("    | |    ")
+    print("    | |    ")
+    print("    |_|    ")
+
+def u():
+    print("   __ __   ")
+    print("  | | | |  ")
+    print("  | | | |  ")
+    print("  | |_| |  ")
+    print("   \___/   ")
+
+def v():
+    print("  __     __")
+    print("  \ \   / /")
+    print("   \ \ / / ")
+    print("    \ V /  ")
+    print("     \_/   ")
+
+def w():
+    print("  __        __ ")
+    print("  \ \      / / ")
+    print("   \ \ /\ / /  ")
+    print("    \ V  V /   ")
+    print("     \_/\_/    ")
+
+def x():
+    print("  __  __ ")
+    print("  \ \/ / ")
+    print("   \  /  ")
+    print("   /  \  ")
+    print("  /_/\_\ ")
+
+def y():
+    print("  __   __  ")
+    print("  \ \ / /  ")
+    print("   \ V /   ")
+    print("    | |    ")
+    print("    |_|    ")
+
+def z():
+    print("  ______  ")
+    print("  |__  / ")
+    print("    / /  ")
+    print("   / /_  ")
+    print("  /____| ")
+
+# chama as letras
+p()
+r()
+o()
+g()
+r()
+a()
+m()
+e()
